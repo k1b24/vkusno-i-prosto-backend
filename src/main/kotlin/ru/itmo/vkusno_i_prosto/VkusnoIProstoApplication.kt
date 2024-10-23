7b61daeff6774412d76b34e6ef250dedca77e020
s.lazeev <s.lazeev@okko.tv>
2024-10-23 18:14:10 +0300

    init

---
+++ src/main/kotlin/ru/itmo/vkusno_i_prosto/VkusnoIProstoApplication.kt
@@ -0,0 +1,11 @@
+package ru.itmo.vkusno_i_prosto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VkusnoIProstoApplication
+
+fun main(args: Array<String>) {
+	runApplication<VkusnoIProstoApplication>(*args)
+}
---
+++ src/test/kotlin/ru/itmo/vkusno_i_prosto/VkusnoIProstoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.itmo.vkusno_i_prosto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VkusnoIProstoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

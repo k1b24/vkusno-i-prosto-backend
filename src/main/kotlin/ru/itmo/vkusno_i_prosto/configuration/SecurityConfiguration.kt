73864f592b479a2bf0a5b0a10d3ae230d3a03d80
s.lazeev <s.lazeev@okko.tv>
2024-11-19 01:56:35 +0300

    try fix cors policy

--- src/main/kotlin/ru/itmo/vkusno_i_prosto/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/ru/itmo/vkusno_i_prosto/configuration/SecurityConfiguration.kt
@@ -2,12 +2,16 @@ package ru.itmo.vkusno_i_prosto.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
 import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import java.util.*
 
@@ -25,6 +29,7 @@ class SecurityConfiguration(
             it.requestMatchers("/v1/token").permitAll()
             it.requestMatchers("/error").permitAll()
             it.requestMatchers("/v1/recipes").permitAll()
+            it.requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/v1/recipes/**")).permitAll()
             it.anyRequest().authenticated()
         }
         .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
@@ -34,12 +39,12 @@ class SecurityConfiguration(
     }
 
     @Bean
-    fun corsConfigurationSource(): UrlBasedCorsConfigurationSource {
+    fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
         configuration.allowCredentials = true
         configuration.allowedHeaders = listOf("*")
         configuration.allowedOriginPatterns = Collections.singletonList("*")
-        configuration.allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH", "HEAD")
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
         return source
--- src/main/kotlin/ru/itmo/vkusno_i_prosto/controllers/RecipesController.kt
+++ src/main/kotlin/ru/itmo/vkusno_i_prosto/controllers/RecipesController.kt
@@ -1,8 +1,10 @@
 package ru.itmo.vkusno_i_prosto.controllers
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -118,6 +120,11 @@ class RecipesController(
             throw ResponseStatusException(HttpStatus.FORBIDDEN.value(), "You are not the owner of this recipe")
         }
         recipesRepository.deleteById(recipeId)
+    }
+
+    @ExceptionHandler(ResponseStatusException::class)
+    fun handleResponseStatusException(e: ResponseStatusException): ResponseEntity<ResponseStatusException> {
+        return ResponseEntity.status(e.code).body(e)
     }
 }
 

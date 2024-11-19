c840f1630f25692acf878f6f217bef383ed0119f
s.lazeev <s.lazeev@okko.tv>
2024-11-19 13:09:09 +0300

    fix cors2

--- src/main/kotlin/ru/itmo/vkusno_i_prosto/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/ru/itmo/vkusno_i_prosto/configuration/SecurityConfiguration.kt
@@ -45,7 +45,7 @@ class SecurityConfiguration(
         configuration.allowedHeaders = listOf("*")
         configuration.allowedMethods = listOf("http://localhost:8080", "http://localhost:5173", "http://147.45.165.69:5173")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH", "HEAD")
-        configuration.allowedOrigins = Collections.singletonList("http://localhost:8080", "http://localhost:5173", "http://147.45.165.69:5173")
+        configuration.allowedOrigins = listOf("http://localhost:8080", "http://localhost:5173", "http://147.45.165.69:5173")
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
         return source
